a1f73f5374fd569ac72a79b2aee1a926461c8d0c
Sergei Protasov <protas1331@icloud.com>
2023-07-27 12:02:28 +0700

    rebase

--- build.sh
+++ build.sh
@@ -1,16 +1,12 @@
-PROJECT_DIR=/run/media/purpflow/Elements/Sources/kpurple
 
-cd $PROJECT_DIR
 
 function asm_64 () {
-    AARCH64_TOOLS=/run/media/purpflow/Elements/Applications/Linux/gcc-arm-10.3-2021.07-x86_64-aarch64-none-elf/bin
-
-    $AARCH64_TOOLS/aarch64-none-elf-as -c src/bootload/boot-aarch64.S -o tmp/boot.o
-    $AARCH64_TOOLS/aarch64-none-elf-gcc -ffreestanding \
+    aarch64-none-elf-as -c src/bootload/boot-aarch64.S -o tmp/boot.o
+    aarch64-none-elf-gcc -ffreestanding \
         -c src/kernel.c -o tmp/kernel.o -O2 -Wall -Wextra
-    $AARCH64_TOOLS/aarch64-none-elf-gcc -T src/linker64.ld -o tmp/kernel.elf \
+    aarch64-none-elf-gcc -T src/linker64.ld -o tmp/kernel.elf \
         -ffreestanding -O2 -nostdlib tmp/boot.o tmp/kernel.o -lgcc
-    $AARCH64_TOOLS/aarch64-none-elf-objcopy tmp/kernel.elf -O binary bin/kernel-aarch64.img 
+    aarch64-none-elf-objcopy tmp/kernel.elf -O binary bin/kernel-aarch64.img 
 }
 
 asm_64
