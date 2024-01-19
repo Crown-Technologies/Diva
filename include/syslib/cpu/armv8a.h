48012982d1c88a2b2442c6acf49dd9e5a7eae8a7
Alexandra Shishkina <protas1331@icloud.com>
2024-01-20 02:23:13 +0700

    Funcs are defines now

--- include/syslib/cpu/armv8a.h
+++ include/syslib/cpu/armv8a.h
@@ -3,34 +3,17 @@
 
 #include <ntypes.h>
 
+#define nop() asm volatile("nop")
+#define sev() asm volatile("sev")
+#define wfe() asm volatile("wfe")
+#define isb() asm volatile("isb")
+#define enable_irq() asm volatile("msr daifclr, #0x2" ::: "memory")
+#define disable_irq() asm volatile("msr daifset, #0x2" ::: "memory")
+
 static inline unsigned long long cpuid() {
     unsigned long long mpidr;
     asm volatile("mrs %0, mpidr_el1" : "=r"(mpidr));
     return mpidr & 0xff;
-}
-
-static inline void nop() {
-    asm volatile("nop");
-}
-
-static inline void sev() {
-    asm volatile("sev");
-}
-
-static inline void wfe() {
-    asm volatile("wfe");
-}
-
-static inline void isb() {
-    asm volatile("isb");
-}
-
-static inline void enable_irq() {
-    asm volatile("msr daifclr, #0x2" ::: "memory");
-}
-
-static inline void disable_irq() {
-    asm volatile("msr daifset, #0x2" ::: "memory");
 }
 
 static inline u64 get_el() {
