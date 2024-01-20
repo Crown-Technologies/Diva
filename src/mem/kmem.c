6251514d72735889ecbe0aaf71c9b2bd8afed9f6
Alexandra Shishkina <protas1331@icloud.com>
2024-01-20 15:49:56 +0700

    kalloc_info is located in heap and grows to lower addresses.

--- src/mem/kmem.c
+++ src/mem/kmem.c
@@ -1,24 +1,18 @@
 #include <stddef.h>
 #include <ntypes.h>
-#include <stdlib/mem.h>
 #include <spinlock.h>
-#include <diva/kernel.h>
-#include <stdlib/time.h>
-
-#include <driver/uart.h>
 
 extern volatile u8 _end;
 
-#define HEAP_START   (u64)&_end + 8
-#define HEAP_END     (1024 * 1024 * 1024 + 256)   /* 1024 MB */
-
-#define MAX_ALLOC_ALLOWED 1024
-
+#define HEAP_START ((u64)&_end + 8)
+#define HEAP_END   (1024 * 1024 * 1024 - 16) /* 1024 MB */
 
 struct kalloc_info {
     u64 addr;
     u64 size; 
-} kalloc_info[MAX_ALLOC_ALLOWED];
+} *kalloc_info = (struct kalloc_info*) HEAP_END - sizeof(struct kalloc_info);
+
+u64 bottom_border_heap_info = HEAP_END - sizeof(struct kalloc_info);
 
 struct spinlock kalloc_lk;
 
@@ -29,23 +23,25 @@ void *kmalloc(unsigned long long size) {
     
     u64 addr = HEAP_START;
 
-    for (u16 i = 0; i < MAX_ALLOC_ALLOWED; i++) {
-        if (kalloc_info[i].addr != 0 &&
-            (addr <= kalloc_info[i].addr < addr + size ||
-            addr <= kalloc_info[i].addr + kalloc_info[i].size < addr + size)) {
-                addr = kalloc_info[i].addr + kalloc_info[i].size;
+    for (struct kalloc_info* ptr = (struct kalloc_info*) HEAP_END; (u64) ptr > bottom_border_heap_info; ptr--) {
+        if (ptr->addr != 0 &&
+            (addr <= ptr->addr < addr + size ||
+            addr <= ptr->addr + ptr->size < addr + size)) {
+                addr = ptr->addr + ptr->size;
             }
     }
 
-    if (addr + size > HEAP_END) {
+    if (addr + size > bottom_border_heap_info) {
         release(&kalloc_lk);
         return NULL;
     }
 
-    for (u16 i = 0; i < MAX_ALLOC_ALLOWED; i++) {
-        if (kalloc_info[i].addr == 0) {
-            kalloc_info[i].addr = addr;
-            kalloc_info[i].size = size;
+    for (struct kalloc_info* ptr = (struct kalloc_info*) HEAP_END; (u64) ptr > bottom_border_heap_info; ptr--) {
+        if (ptr->addr == 0) {
+            ptr->addr = addr;
+            ptr->size = size;
+            bottom_border_heap_info--;
+            ptr[-1].addr = 0;
 
             release(&kalloc_lk);
 
@@ -65,9 +61,9 @@ void kfree(void *va) {
 
     acquire(&kalloc_lk);
 
-    for (u16 i = 1; i < MAX_ALLOC_ALLOWED; i++)
-        if (va == (void*) kalloc_info[i].addr)
-            kalloc_info[i].addr = 0;
+    for (struct kalloc_info* ptr = (struct kalloc_info*) HEAP_END; (u64) ptr > bottom_border_heap_info; ptr--)
+        if (va == (void*) ptr->addr)
+            ptr->addr = 0;
 
     release(&kalloc_lk);
 }
@@ -77,7 +73,5 @@ void premmu_alloc_init() {
     lock_init(&kalloc_lk);
     kalloc_info[0].size = 0;
     kalloc_info[0].addr = HEAP_START;
-    for (u16 i = 1; i < MAX_ALLOC_ALLOWED; i++)
-        kalloc_info[i].addr = 0;
 }
 
